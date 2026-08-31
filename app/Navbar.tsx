f93d78a82c9aab16d2b59e30dbe7dbd57989e84c
Ryan Ribeiro <ryanribeirocontato@outlook.com>
2026-08-30 21:07:31 -0300

    adição de funcionalidades e layout da página principal

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,14 +1,23 @@
 import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { Users, Zap, Target } from "lucide-react";
 
 export default function Navbar() {
     return (
-        <nav className="font-jet flex flex-row p-8 justify-between bg-[#F1FAEE] text-[#000000]">
-            <div className="flex flex-row gap-6">
-                <Link href={"/"}>Início</Link>
-                <Link href={"/dashboard"}>Dashboard</Link>
-                <Link href={"/leads"}>Leads</Link>
+        <header className="w-full border-b border-white/30 bg-transparent backdrop-blur-md">
+            <nav className="font-jet flex flex-row p-8 justify-between items-center max-w-7xl mx-auto p-4">
+            <div className="flex gap-6">
+                    <Link href={"/"} className="flex gap-2 tracking-tight font-bold whitespace-nowrap"><Target /><span>Mini-CRM</span></Link>
+                <div className="hidden sm:flex flex-row gap-6">
+                    <Link href={"/dashboard"}>Dashboard</Link>
+                    <Link href={"/leads"}>Leads</Link>
+                </div>
             </div>
-            <Link href={"/login"}>Login</Link>
+            <Button variant="outline" size="lg">
+                <Zap />
+                <Link href={"/login"}>Login/Cadastro</Link>
+            </Button>
         </nav>
+        </header>
     );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
 import Image from "next/image";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="font-jet font-bold flex-1 w-full flex flex-col justify-center items-center">
-      <h1 className="text-4xl md:text-6xl">Mini Leads CRM</h1>
+    <main className="flex-1 gap-3 w-full flex flex-col justify-center items-center px-10">
+      <Badge variant="ghost" className="font-jet text-md">🚀 Gestão simples para pequenos negócios</Badge>
+      <h1 className="font-jet font-bold text-4xl md:text-5xl bg-gradient-to-r from-white via-slate-200 to-slate-400 bg-clip-text text-transparent">Mini Leads CRM</h1>
+      <p className="text-slate-400 text-base md:text-xl max-w-2xl leading-relaxed text-center">Organize seus potenciais clientes, envie propostas e feche negócios mais rápido em uma única plataforma.</p>
+      <Button variant="outline" className="!bg-emerald-600 hover:!bg-emerald-500">
+        <Link href={"/login"} className="font-jet">
+        Acessar Plataforma
+        </Link>
+      </Button>
     </main>
   );
 }
